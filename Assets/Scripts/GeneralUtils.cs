a5158642760ddaddefb6f45db9bb4b001f9c092f
Vwing <vwing@uci.edu>
2016-02-25 16:37:39 -0800

    LeapMotion networking works!
    Added a SyncChildTransforms script to add/remove the sync script to each
    child from the editor. It works flawlessly!!

---
+++ Assets/Scripts/GeneralUtils.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ * I'll be transferring these scripts to my library sooner or later.
+ * It's just faster to make them here on the fly.
+ * 
+ * **/
+public class GeneralUtils : MonoBehaviour 
+{
+}
---
+++ Assets/Scripts/SyncChildTransforms.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SyncChildTransforms : MonoBehaviour 
+{
+    //[Header("Network IDs")]
+    [Tooltip("Script sets packet IDs starting at this value, and iterates forward for each child")]
+    public int startID = 0;
+    [Tooltip("Don't bother setting this. It's just so you know what the last ID is once it's set by the script")]
+    public int endID;
+    private int id;
+    //[Header("SyncTransform variables")]
+    public bool receiving;
+    public bool useLocalValues = true;
+    public bool SyncPosition = true;
+    public bool SyncRotation = true;
+    public bool SyncScale = true;
+
+    //uses DepthFirstSearch to add SyncTransform script to each child
+    [ContextMenu("Add SyncTransforms to all children")]
+	public void AddSyncTransforms () 
+    {
+        id = startID;
+        Transform[] children = GetComponentsInChildren<Transform>();
+        Debug.Log("Length: " + children.Length);
+        for (int i = 0; i < children.Length; ++ i)
+        {
+            SyncTransformation sync = children[i].gameObject.AddComponent<SyncTransformation>();
+            sync.receiving = receiving;
+            sync.useLocalValues = useLocalValues;
+            sync.SyncPosition = SyncPosition;
+            sync.SyncRotation = SyncRotation;
+            sync.SyncScale = SyncScale;
+            sync.networkID = id++;
+        }
+        endID = id;
+	}
+
+    [ContextMenu("Remove SyncTransforms from all children")]
+    public void RemoveSyncTransforms()
+    {
+        SyncTransformation[] syncs = transform.GetComponentsInChildren<SyncTransformation>();
+        Debug.Log("Sync Length: " + syncs.Length);
+        foreach (SyncTransformation s in syncs)
+            DestroyImmediate(s);
+        endID = startID;
+    }
+}
