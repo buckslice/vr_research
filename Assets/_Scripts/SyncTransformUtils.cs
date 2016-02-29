c4d90d5524e6c6caf8756e7e797aa95b9a14b4e6
Vwing <vwing@uci.edu>
2016-02-28 23:10:25 -0800

    Made networked leap motion work for real
    Turns out, what I was seeing was actually the leap motion tracking my
    hand in both editors. It uses weird code to move the mesh, so I just
    ended up copy-pasting primitive shapes over everything and networking
    those. Works now.

--- Assets/_Scripts/SyncTransformUtils.cs
+++ Assets/_Scripts/SyncTransformUtils.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SyncChildTransforms : MonoBehaviour 
+public class SyncTransformUtils : MonoBehaviour 
 {
-    //[Header("Network IDs")]
-    [Tooltip("Script sets packet IDs starting at this value, and iterates forward for each child")]
+    [Header("Network IDs")]
+    [Tooltip("AddSyncTransforms sets packet IDs starting at this value, and iterates forward for each child")]
     public int startID = 0;
-    [Tooltip("Don't bother setting this. It's just so you know what the last ID is once it's set by the script")]
+    [Tooltip("Don't bother setting this. It's just so you know what the last ID is once it's set by the AddSyncTransfroms")]
     public int endID;
     private int id;
-    //[Header("SyncTransform variables")]
+    [Header("SyncTransform variables")]
     public bool receiving;
     public bool useLocalValues = true;
     public bool SyncPosition = true;
     public bool SyncRotation = true;
     public bool SyncScale = true;
+    public float epsilon = 0.01f;
 
     //uses DepthFirstSearch to add SyncTransform script to each child
     [ContextMenu("Add SyncTransforms to all children")]
@@ -44,5 +45,32 @@ public class SyncChildTransforms : MonoBehaviour
         foreach (SyncTransformation s in syncs)
             DestroyImmediate(s);
         endID = startID;
+    }
+
+    [ContextMenu("Log the total count of SyncTransforms in the scene")]
+    public void CountSyncTransformsInScene()
+    {
+        SyncTransformation[] syncs = Object.FindObjectsOfType<SyncTransformation>();
+        int lowestID = int.MaxValue;
+        int highestID = 0;
+        foreach(SyncTransformation s in syncs)
+        {
+            if (s.networkID < lowestID)
+                lowestID = s.networkID;
+            if (s.networkID > highestID)
+                highestID = s.networkID;
+        }
+
+        Debug.Log("Number of syncs: " + syncs.Length);
+        Debug.Log("Lowest ID: " + lowestID);
+        Debug.Log("Highest ID: " + highestID);
+    }
+
+    [ContextMenu("Update the epsilon of each SyncTransform in the scene")]
+    public void UpdateEpsilons()
+    {
+        SyncTransformation[] syncs = Object.FindObjectsOfType<SyncTransformation>();
+        foreach (SyncTransformation s in syncs)
+            s.epsilon = epsilon;
     }
 }
