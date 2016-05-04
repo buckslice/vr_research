a0cde37dff73a49817c3586de07f274b58261e53
Vwing <vwing@uci.edu>
2016-05-04 13:39:29 -0700

    Added interaction btw image targets

---
+++ Assets/_Scripts/MoleculeBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoleculeBehavior : MonoBehaviour
+{
+	public Vector3 destination = Vector3.zero;
+	public float speed = 1f;
+	private bool destinationReached = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if(!destinationReached && Vector3.Distance (transform.localPosition, destination) < .1f * speed)
+			destinationReached = true;
+		if(!destinationReached)
+			transform.localPosition -= (transform.localPosition - destination).normalized * speed * Time.deltaTime;
+		if(destinationReached)
+			Destroy(this.gameObject);
+	}
+}
---
+++ Assets/_Scripts/SendMolecules.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendMolecules : MonoBehaviour
+{
+	public GameObject molecule;
+	public GameObject destinationOrganelle;
+	private Renderer renderer;
+	public float spawnInterval = 3f;
+	private float timer = 0;
+	void Start ()
+	{
+		renderer = GetComponent<Renderer>();
+	}
+	
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		if(renderer.enabled && timer > spawnInterval && destinationOrganelle.activeSelf)
+		{
+			GameObject newMolecule = Instantiate(molecule, transform.position, transform.rotation) as GameObject;
+			MoleculeBehavior mol = newMolecule.GetComponent<MoleculeBehavior>();
+			mol.destination = transform.localPosition;
+			newMolecule.transform.parent = destinationOrganelle.transform;
+			timer = 0f;
+		}
+	}
+}
