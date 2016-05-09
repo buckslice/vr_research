724af2a563b2cd0e2ab7b4497f5b5fc922c01d74
Vwing <vwing@uci.edu>
2016-05-09 14:19:38 -0700

    Lined up hands more accurately
    And made it possible to change them on the fly from the editor

---
+++ Assets/_Scripts/DisableProxy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableProxy : MonoBehaviour {
+    public GameObject realHandLeft;
+    public GameObject realHandRight;
+    public GameObject proxyHandLeft;
+    public GameObject proxyHandRight;
+
+    bool valuesSet;
+	// Use this for initialization
+	void Start () {
+        valuesSet = ValsAreSet();
+        if (!valuesSet)
+            Debug.Log("DisableProxy: Set real and proxy hands so this script works");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        if (valuesSet)
+        {
+            proxyHandLeft.SetActive(realHandLeft.activeSelf);
+            proxyHandRight.SetActive(realHandRight.activeSelf);
+        }
+        else
+            valuesSet = ValsAreSet();
+	}
+
+    bool ValsAreSet()
+    {
+        return !(!realHandLeft || !realHandRight || !proxyHandLeft || !proxyHandRight);
+    }
+}
