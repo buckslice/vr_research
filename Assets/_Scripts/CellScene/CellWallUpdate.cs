f106403d6f065ba012dafdc64d6d1cf18b3664b2
jfc3 <buckslice@gmail.com>
2016-05-13 20:22:00 -0700

    Updated cell shader for mitochondria, made cell generation happen offline

---
+++ Assets/_Scripts/CellScene/CellWallUpdate.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CellWallUpdate : MonoBehaviour {
+
+    public float cellRadius = 25.0f;
+    public float rippleScale = 1.0f;
+    public bool fadeWhenClose = true;
+
+    private MeshRenderer mr;
+
+    // Use this for initialization
+    void Start () {
+        mr = GetComponent<MeshRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        mpb.SetFloat("_RippleScale", rippleScale);
+
+        if (fadeWhenClose) {
+            float d = Vector3.Magnitude(Camera.main.transform.position - transform.position);
+            float min = cellRadius * 2.0f;
+            float max = min + 20.0f;
+            float t = (d - min) / (min - max);
+            float a = Mathf.Lerp(0.1f, 0.5f, 1.0f - t);
+            Color c = mr.material.GetColor("_Color");
+            c.a = a;
+            mpb.SetColor("_Color", c);
+        }
+
+        mr.SetPropertyBlock(mpb);
+    }
+}
